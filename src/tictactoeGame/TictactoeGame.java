6331f53860e8a013302204d4e6a53390a694b377
unknown <ompreetam317@gmail.com>
2020-10-07 09:54:07 +0530

    [Preetam]Added:UC2-Allowing User Input

--- src/tictactoeGame/TictactoeGame.java
+++ src/tictactoeGame/TictactoeGame.java
@@ -1,6 +1,9 @@
 package tictactoeGame;
 
+import java.util.Scanner;
+
 public class TictactoeGame {
+	/* UC1 */
 	public static char[] createBoard() {
 		char[] board = new char[10];
 		for (int position = 0; position < board.length; position++) {
@@ -9,8 +12,17 @@ public class TictactoeGame {
 		return board;
 	}
 
+	/* UC2 */
+	public static void chooseXorO() {
+		System.out.println("Enter X or O");
+		Scanner myObj = new Scanner(System.in);
+		String userInput = myObj.nextLine();
+		char c=userInput.charAt(0);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome To TicTAcToe Problem ");
+		System.out.println("Welcome To TicTAcToe Game ");
 		char[] board = createBoard();
+		chooseXorO();
 	}
 }
