4fde2cd4ec9f8dddd3ed574885754f701d540aea
unknown <ompreetam317@gmail.com>
2020-10-07 09:35:18 +0530

    [Preetam]Added:UC1-Creating Board

--- src/tictactoeGame/TictactoeGame.java
+++ src/tictactoeGame/TictactoeGame.java
@@ -1,9 +1,16 @@
 package tictactoeGame;
 
 public class TictactoeGame {
-	
+	public static char[] createBoard() {
+		char[] board = new char[10];
+		for (int position = 0; position < board.length; position++) {
+			board[position] = ' ';
+		}
+		return board;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To TicTAcToe Problem ");
+		char[] board = createBoard();
 	}
 }
