7f78daa4e4f91870e97d0741fc7db5be6f6d8b68
unknown <ompreetam317@gmail.com>
2020-10-07 10:37:01 +0530

    [Preetam]Added:UC3 feature to display current board

--- src/tictactoeGame/TictactoeGame.java
+++ src/tictactoeGame/TictactoeGame.java
@@ -3,9 +3,11 @@ package tictactoeGame;
 import java.util.Scanner;
 
 public class TictactoeGame {
+	private static char[] board = new char[10];
+
 	/* UC1 */
 	public static char[] createBoard() {
-		char[] board = new char[10];
+
 		for (int position = 0; position < board.length; position++) {
 			board[position] = ' ';
 		}
@@ -17,14 +19,22 @@ public class TictactoeGame {
 		System.out.println("Enter X or O");
 		Scanner myObj = new Scanner(System.in);
 		String userInput = myObj.nextLine();
-		char UserInput = userInput.charAt(0);
+		char UserInput = userInput.toUpperCase().charAt(0);
 		myObj.close();
 		return UserInput;
+	}
+	/*UC3*/
+	public static void displayBoard() {
+		for (int position = 0; position < board.length; position++) {
+			System.out.println(board[position]);
+		}
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To TicTAcToe Game ");
 		char[] board = createBoard();
-		char UserInput = chooseSymbol();
+		char userLetter = chooseSymbol();
+		char computerLetter = (userLetter == 'X') ? '0' : 'X';
+
 	}
 }
